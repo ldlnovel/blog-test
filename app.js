5cb5f0968f106c77fc735af0030e21cb55edb5f9
lnovel <dongliang.li@youhualin.com>
2018-12-20 17:53:17 +0800

    ---

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-
 var app = express();
 
 // view engine setup
@@ -20,8 +19,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+
+app.use('/aaa', indexRouter);
 app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
